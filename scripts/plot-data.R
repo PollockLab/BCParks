71c9a95fd770688dd78c4f4af240e154a011c388
Katherine Hébert <katherine.ahebert@gmail.com>
2026-01-22 14:54:37 +0100

    save scores and plots

--- scripts/plot-data.R
+++ scripts/plot-data.R
@@ -6,6 +6,9 @@ library(tidyverse)
 library(rgbif)
 library(scales)
 
+#theme_set(hrbrthemes::theme_ipsum_rc())
+theme_set(theme_minimal())
+
 # Load data --------------------------------------------------------------------
 
 # load base grid
@@ -62,7 +65,7 @@ obs = project(obs, crs(parks5k))
 
 # Density map ------------------------------------------------------------------
 
-map_obsdens = rasterize(obs, parks5k, fun = "length")
+map_obsdens = rasterize(obs, parks5k, fun = "length") |> sqrt()
 map_obsdens[is.na(map_obsdens)] <- 0
 map_obsdens[is.na(parks5k)] <- NA
 plot(map_obsdens)
@@ -70,7 +73,7 @@ plot(map_obsdens)
 # Last update map --------------------------------------------------------------
 
 map_lastupdate = rasterize(obs, parks5k, field = "year", fun = "max")
-map_lastupdate[is.na(map_lastupdate)] <- 0
+map_lastupdate[is.na(map_lastupdate)] <- min(obs$year, na.rm = TRUE)
 map_lastupdate[is.na(parks5k)] <- NA
 plot(map_lastupdate)
 
@@ -103,6 +106,24 @@ clim_freq[is.na(parks5k)] <- NA
 plot(clim_freq)
 # save the map
 map_climfrequency = clim_freq
+
+# plot to demo the approach
+ggplot() +
+  geom_point(data = clim_df, 
+             aes(x = wc2.1_10m_bio_1, y = wc2.1_10m_bio_12),
+             alpha = .4, col = "steelblue3") +
+  geom_hline(yintercept = y_breaks, linewidth = .1) +
+  geom_vline(xintercept = x_breaks, linewidth = .1) +
+  theme_classic() 
+ggsave("figures/clim_grid_bins.png", width = 5, height = 5)
+ggplot() +
+  geom_bin2d(data = clim_df, 
+             aes(x = wc2.1_10m_bio_1, y = wc2.1_10m_bio_12), 
+             bins = n_bins, alpha = 1,) +
+  scale_fill_viridis_c(option = "plasma") +
+  theme_classic() +
+  labs(fill = "Count")
+ggsave("figures/clim_grid_count.png", width = 5, height = 5)
 
 
 # Completeness score -----------------------------------------------------------
@@ -148,7 +169,51 @@ for(i in 2:ncol(scores_weighted)){
 scores_m = as.matrix(scores_weighted[,-1])
 scores_total = rowSums(scores_m)
 
+# make a clean table
+scores_df = scores_scaled
+scores_df$total = scores_total
+write.csv(scores_df, "outputs/scores_spatial.csv")
+
+# save output
+saveRDS(list("scaled" = scores_scaled,
+             "weighted" = scores_weighted,
+             "total" = scores_total), "outputs/spatial-layers/scores_spatial.rds")
+
+
 # map the scores 
 map_scores = parks5k
 map_scores[parks5k==1] <- scores_total
 plot(map_scores)
+
+# pretty maps ===================================================================
+
+ggplot() +
+  tidyterra::geom_spatraster(data = map_scores) +
+  scale_fill_viridis_c(option = "turbo", limits = c(0,1),
+                       na.value = "transparent",name = "VOI") +
+  labs(title = "Total spatial priority score")
+ggsave("figures/scores_spatialpriorities.png", width = 9, height = 8)
+
+# save maps of the scaled scores
+map_spatial = c("obsdensity" = map_scores, 
+                "lastupdate" = map_scores, 
+                "climfreq" = map_scores)
+map_spatial$obsdensity[parks5k==1] <- scores_scaled$score_spatial_density
+map_spatial$lastupdate[parks5k==1] <- scores_scaled$score_spatial_lastupdate
+map_spatial$climfreq[parks5k==1] <- scores_scaled$score_spatial_climfreq
+map_spatial$total = map_scores
+saveRDS(map_spatial, "outputs/spatial-layers/scores_map_spatial.rds")
+
+p = list()
+for(i in 1:length(map_spatial)){
+  p[[i]] = ggplot() +
+    tidyterra::geom_spatraster(data = map_spatial[[i]]) +
+    scale_fill_viridis_c(option = "plasma", limits = c(0,1), direction = -1,
+                         na.value = "transparent", name = "VOI") +
+    labs(title = names(map_spatial)[i])
+  ggsave(plot = p[[i]], filename = paste0("figures/scores_spatialpriorities_",names(map_spatial)[i],".png"), width = 9, height = 8)
+  
+}
+patchwork::wrap_plots(p, nrow = 2, ncol = 2)
+ggsave("figures/scores_spatialpriorities_eachscore.png", width = 9, height = 8)
+
